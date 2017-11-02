e58e1d19a4cab6fbae075e93839ab122f9a541e6
robinchoii <robinchoii@gmail.com>
2017-11-02 14:57:58 -0700

    add counter component, hot module replacement

---
+++ my-app/src/components/Counter.vue
@@ -0,0 +1,36 @@
+<template>
+
+    <div>
+
+        <h1>Count: {{count}}</h1>
+
+        <button @click='count += 11'>increase count</button>
+    </div>
+
+</template>
+
+<script type="text/javascript">
+
+    export default {
+
+        name: 'app',
+
+        data() {
+
+            return {
+
+            count: 0
+
+            }
+
+        }
+
+    }
+
+</script>
+
+<style type="text/css">
+
+
+
+</style>
