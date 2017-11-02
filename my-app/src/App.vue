16638a8555fc2e3aff699d9d51a4abf9167a4c88
robinchoii <robinchoii@gmail.com>
2017-11-02 14:58:21 -0700

    import counter component'

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -5,18 +5,21 @@
     <message>Hello There!</message>
     <message>Hello {{name}}! </message>
 
+    <counter></counter>
+
   </div>
 
 </template>
 
 <script>
     import Message from './components/Message.vue';
+    import Counter from './components/Counter.vue';
 
     export default {
 
         name: 'app',
 
-        components: {Message},
+        components: {Message, Counter},
 
         data() {
 
