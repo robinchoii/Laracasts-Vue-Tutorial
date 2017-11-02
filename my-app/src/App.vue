e6bf6f2298a34d59a09f091441fe4104fef553cf
robinchoii <robinchoii@gmail.com>
2017-11-02 14:50:37 -0700

    webpack set up and making a new component file

---
+++ my-app/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+
+  <div id="app">
+
+    <message>Hello There!</message>
+    <message>Hello {{name}}! </message>
+
+  </div>
+
+</template>
+
+<script>
+    import Message from './components/Message.vue';
+
+    export default {
+
+        name: 'app',
+
+        components: {Message},
+
+        data() {
+
+            return {
+
+                name: "robin"
+
+            }
+        }
+
+    }
+
+</script>
+
+
+
+<style>
+
+
+
+</style>
